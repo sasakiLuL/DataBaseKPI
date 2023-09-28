607299c9a773b7b1e5876209f60eb2ea5ac38408
Oleksandr <sashakrivko75@gmail.com>
2023-09-28 13:12:35 +0200

    Added test example

--- RGR/RGR.Console/Program.cs
+++ RGR/RGR.Console/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Npgsql;
+
+using NpgsqlConnection connection = new NpgsqlConnection();
+connection.ConnectionString = "host=localhost;port=5433;database=appoi_db;user id=postgres;password=pass12345";
+connection.Open();
+
+using (var cmd = connection.CreateCommand())
+{
+    cmd.CommandText = "SELECT * FROM users";
+
+    using (var reader = cmd.ExecuteReader())
+    {
+        while (reader.Read()) 
+        {
+            Console.WriteLine($"{reader["user_id"]}, {reader["first_name"]}");
+        }
+    }
+}
