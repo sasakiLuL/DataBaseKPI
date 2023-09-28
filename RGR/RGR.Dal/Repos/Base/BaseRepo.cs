6b5a2a5f5718f515e7f0e831d4beb61ae1abeb57
Oleksandr <sashakrivko75@gmail.com>
2023-09-28 12:42:10 +0200

    Istalled Npgsql and added IRepo

---
+++ RGR/RGR.Dal/Repos/Base/BaseRepo.cs
@@ -0,0 +1,48 @@
+﻿//using Npgsql;
+
+//namespace RGR.Dal.Repos.Base
+//{
+//    public abstract class BaseRepo<T> : IRepo<T> where T : new()
+//    {
+//        private readonly bool _disposedConnection;
+//        public NpgsqlConnection Connection { get; private set; }
+//        protected BaseRepo(NpgsqlConnection connection) 
+//        {
+//            _disposedConnection = false;
+//            Connection = connection;
+//        }
+//        protected BaseRepo(string connectionString) : this(new NpgsqlConnection(connectionString))
+//        {
+//            _disposedConnection = true;
+//        }
+//        public void Dispose() 
+//        {
+//            Dispose(true);
+//            GC.SuppressFinalize(this);
+//        }
+//        private bool _isDisposed;
+//        protected virtual void Dispose(bool disposing)
+//        {
+//            if (_isDisposed)
+//                return;
+
+//            if (disposing) 
+//                if (_disposedConnection)
+//                    Connection.Dispose();
+
+//            _isDisposed = true;
+//        }
+//        ~BaseRepo()
+//        {
+//            Dispose(false);
+//        }
+//        public virtual T? Find(int? id)
+//        {
+//            Connection.Open();
+
+//            NpgsqlCommand cmd = Connection.CreateCommand();
+//            cmd.CommandType = System.Data.CommandType.Text;
+//            cmd.CommandText = @"SELECT * ";
+//        }
+//    }
+//}
---
+++ RGR/RGR.Dal/Repos/Base/IRepo.cs
@@ -0,0 +1,16 @@
+﻿namespace RGR.Dal.Repos.Base
+{
+    public interface IRepo<T> : IDisposable
+    {
+        int Add(T entity, bool persist = true);
+        int AddRange(IEnumerable<T> entities, bool persist = true);
+        int Update(T entity, bool persist = true);
+        int UpdateRange(IEnumerable<T> entities, bool persist = true);
+        int Delete(T entity, bool persist = true);
+        int Delete(int id, bool persist = true);
+        int DeleteRange(IEnumerable<T> entities, bool persist = true);
+        T? Find(int? id);
+        IEnumerable<T> FindAll();
+        void ExequteQuery(string query, object[] sqlParametersObjects);
+    }
+}
