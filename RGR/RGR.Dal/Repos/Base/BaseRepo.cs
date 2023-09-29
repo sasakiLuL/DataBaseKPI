bab950ce3fa4ddc29c22f05bd2009b74b5d3a2b4
Oleksandr <sashakrivko75@gmail.com>
2023-09-29 19:03:43 +0200

    Last Repo commit

--- RGR/RGR.Dal/Repos/Base/BaseRepo.cs
+++ RGR/RGR.Dal/Repos/Base/BaseRepo.cs
@@ -1,48 +1,113 @@
-﻿//using Npgsql;
+﻿using Npgsql;
+using System.Data;
 
-//namespace RGR.Dal.Repos.Base
-//{
-//    public abstract class BaseRepo<T> : IRepo<T> where T : new()
-//    {
-//        private readonly bool _disposedConnection;
-//        public NpgsqlConnection Connection { get; private set; }
-//        protected BaseRepo(NpgsqlConnection connection) 
-//        {
-//            _disposedConnection = false;
-//            Connection = connection;
-//        }
-//        protected BaseRepo(string connectionString) : this(new NpgsqlConnection(connectionString))
-//        {
-//            _disposedConnection = true;
-//        }
-//        public void Dispose() 
-//        {
-//            Dispose(true);
-//            GC.SuppressFinalize(this);
-//        }
-//        private bool _isDisposed;
-//        protected virtual void Dispose(bool disposing)
-//        {
-//            if (_isDisposed)
-//                return;
+namespace RGR.Dal.Repos.Base
+{
+    public abstract class BaseRepo<T> : IRepo<T> where T : new()
+    {
+        private readonly bool _disposedConnection;
+        private readonly bool _disposedAdapter;
+        private bool _isDisposed;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_isDisposed)
+                return;
+            if (disposing)
+            {
+                if (_disposedConnection)
+                    _connection.Dispose();
+                if (_disposedAdapter)
+                    _adapter.Dispose();
+            }
+            _isDisposed = true;
+        }
+        ~BaseRepo()
+        {
+            Dispose(false);
+        }
 
-//            if (disposing) 
-//                if (_disposedConnection)
-//                    Connection.Dispose();
+        protected DataSet _set;
+        protected NpgsqlConnection _connection;
+        protected NpgsqlDataAdapter _adapter;
+        protected BaseRepo(NpgsqlConnection connection, params string[] tablesNames)
+        {
+            _connection = connection;
+            _set = new DataSet();
+            _adapter = new NpgsqlDataAdapter();
 
-//            _isDisposed = true;
-//        }
-//        ~BaseRepo()
-//        {
-//            Dispose(false);
-//        }
-//        public virtual T? Find(int? id)
-//        {
-//            Connection.Open();
+            _connection.Open();
 
-//            NpgsqlCommand cmd = Connection.CreateCommand();
-//            cmd.CommandType = System.Data.CommandType.Text;
-//            cmd.CommandText = @"SELECT * ";
-//        }
-//    }
-//}
+            string selectCmd = "";
+
+            foreach (var name in tablesNames)
+            {
+                selectCmd += $"SELECT * FROM {name};";
+            }
+
+            _adapter.SelectCommand = new NpgsqlCommand(selectCmd, _connection);
+
+            _adapter.Fill(_set);
+
+            for(int i = 0; i < _set.Tables.Count; i++)
+            {
+                _set.Tables[i].TableName = tablesNames[i];
+            }
+
+            _connection.Close();
+        }
+        public int Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T? Find(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExequteQuery(string query, object[] sqlParametersObjects)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- RGR/RGR.Dal/Repos/Base/IRepo.cs
+++ RGR/RGR.Dal/Repos/Base/IRepo.cs
@@ -2,13 +2,13 @@
 {
     public interface IRepo<T> : IDisposable
     {
-        int Add(T entity, bool persist = true);
-        int AddRange(IEnumerable<T> entities, bool persist = true);
-        int Update(T entity, bool persist = true);
-        int UpdateRange(IEnumerable<T> entities, bool persist = true);
-        int Delete(T entity, bool persist = true);
-        int Delete(int id, bool persist = true);
-        int DeleteRange(IEnumerable<T> entities, bool persist = true);
+        int Add(T entity);
+        int AddRange(IEnumerable<T> entities);
+        int Update(T entity);
+        int UpdateRange(IEnumerable<T> entities);
+        int Delete(T entity);
+        int Delete(int id);
+        int DeleteRange(IEnumerable<T> entities);
         T? Find(int? id);
         IEnumerable<T> FindAll();
         void ExequteQuery(string query, object[] sqlParametersObjects);
