e1ba089d896da86fa9096d9c8da3f9601677af80
Oleksandr <sashakrivko75@gmail.com>
2023-09-29 22:46:56 +0200

    Added mapper for DataTable and DataRow

---
+++ RGR/RGR.Dal/ORM/PropertyMapHelper.cs
@@ -0,0 +1,60 @@
+﻿using System.Data;
+using System.Reflection;
+
+namespace RGR.Dal.ORM
+{
+    public class PropertyMapHelper
+    {
+        public static void Map(Type type, DataRow row, PropertyInfo prop, object entity)
+        {
+            List<string> columnNames = GetDataNames(type, prop.Name);
+
+            foreach (var columnName in columnNames)
+            {
+                if (!String.IsNullOrWhiteSpace(columnName)
+                    && row.Table.Columns.Contains(columnName))
+                {
+                    var propertyValue = row[columnName];
+                    if (propertyValue != DBNull.Value)
+                    {
+                        ParsePrimitive(prop, entity, row[columnName]);
+                        break;
+                    }
+                }
+            }
+        }
+        public static List<string> GetDataNames(Type type, string propertyName)
+        {
+            var property = type
+                           .GetProperty(propertyName)
+                           .GetCustomAttributes(false)
+                           .Where(x => x.GetType().Name == "DataNamesAttribute")
+                           .FirstOrDefault();
+
+            if (property != null)
+            {
+                return ((DataNamesAttribute)property).ValueNames;
+            }
+            return new List<string>();
+        }
+        private static void ParsePrimitive(PropertyInfo prop, object entity, object value)
+        {
+            if (prop.PropertyType == typeof(string))
+            {
+                prop.SetValue(entity, value.ToString().Trim(), null);
+            }
+            else if (prop.PropertyType == typeof(int)
+                     || prop.PropertyType == typeof(int?))
+            {
+                if (value == null)
+                {
+                    prop.SetValue(entity, null, null);
+                }
+                else
+                {
+                    prop.SetValue(entity, int.Parse(value.ToString()), null);
+                }
+            }
+        }
+    }
+}
