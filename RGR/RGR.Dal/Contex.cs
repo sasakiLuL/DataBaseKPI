c4045b231260bef39b261142fca21447a5f6ef5f
Oleksandr <sashakrivko75@gmail.com>
2023-09-30 23:08:24 +0200

    Added context

---
+++ RGR/RGR.Dal/Contex.cs
@@ -0,0 +1,73 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+using System.Data;
+
+namespace RGR.Dal
+{
+    public class Contex
+    {
+        protected NpgsqlConnection _connection;
+        public Contex(NpgsqlConnection connection)
+        {
+            _connection = connection;
+        }
+        public void CommandNoQuery(string query, params (string name, NpgsqlDbType type, object value)[] paraments)
+        {
+            _connection.Open();
+
+            using var command = _connection.CreateCommand();
+            command.CommandText = query;
+            command.CommandType = CommandType.Text;
+            foreach (var param in paraments)
+            {
+                var dbParam = new NpgsqlParameter()
+                {
+                    ParameterName = param.name,
+                    Value = param.value,
+                    NpgsqlDbType = param.type
+                };
+                command.Parameters.Add(dbParam);
+            }
+
+            command.ExecuteNonQuery();
+
+            _connection.Close();
+        }
+        public IEnumerable<Dictionary<string, object>> Command(string query, params (string name, NpgsqlDbType type, object value)[] paraments)
+        {
+            var outList = new List<Dictionary<string, object>>();
+
+            _connection.Open();
+
+            using var command = _connection.CreateCommand();
+            command.CommandText = query;
+            command.CommandType = CommandType.Text;
+            foreach (var param in paraments)
+            {
+                var dbParam = new NpgsqlParameter()
+                {
+                    ParameterName = param.name,
+                    Value = param.value,
+                    NpgsqlDbType = param.type
+                };
+                command.Parameters.Add(dbParam);
+            }
+
+            using var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var dict = new Dictionary<string, object>();
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    dict.Add(reader.GetName(i), reader.GetValue(i));
+                }
+                outList.Add(dict);
+            }
+
+            _connection.Close();
+
+            return outList;
+        }
+    }
+}
--- RGR/RGR.Dal/Repos/Base/BaseRepo.cs
+++ RGR/RGR.Dal/Repos/Base/BaseRepo.cs
@@ -5,38 +5,18 @@ namespace RGR.Dal.Repos.Base
 {
     public abstract class BaseRepo<T> : IRepo<T> where T : new()
     {
-        protected DataSet _set;
         protected NpgsqlConnection _connection;
-        protected NpgsqlDataAdapter _adapter;
-        protected BaseRepo(NpgsqlConnection connection, params string[] tablesNames)
+        protected BaseRepo(Contex contex)
         {
             _connection = connection;
-            _set = new DataSet();
-            _adapter = new NpgsqlDataAdapter();
-
-            _connection.Open();
-
-            string selectCmd = "";
-
-            foreach (var name in tablesNames)
-            {
-                selectCmd += $"SELECT * FROM {name};";
-            }
-
-            _adapter.SelectCommand = new NpgsqlCommand(selectCmd, _connection);
-
-            _adapter.Fill(_set);
-
-            for(int i = 0; i < _set.Tables.Count; i++)
-            {
-                _set.Tables[i].TableName = tablesNames[i];
-            }
-
-            _connection.Close();
         }
         public int Add(T entity)
         {
-            throw new NotImplementedException();
+            _connection.Open();
+
+            using var command = new NpgsqlCommand();
+            command.CommandType = CommandType.Text;
+
         }
 
         public int AddRange(IEnumerable<T> entities)
