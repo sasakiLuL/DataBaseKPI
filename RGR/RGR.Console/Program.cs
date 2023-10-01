d5c778e7718d00de6c73599bbe144fddf72df40a
Oleksandr <sashakrivko75@gmail.com>
2023-10-01 18:25:48 +0200

    Created Select method

--- RGR/RGR.Console/Program.cs
+++ RGR/RGR.Console/Program.cs
@@ -7,25 +7,54 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq.Expressions;
 using System.Reflection;
 
-var res = Select((Gym e) => e.HomePage);
+var res = Select((Gym e) => new { e.GymName, e.PhoneNumber, e.Email });
+
+Console.ReadLine();
+
 static IList<TResult> Select<TEntity, TResult>(Expression<Func<TEntity, TResult>> selector)
 {
     var connection = new NpgsqlConnection("host=localhost;port=5433;database=appoi_db;user id=postgres;password=pass12345");
 
     var result = new List<TResult>();
 
-    var member = (selector.Body.ReduceExtensions() as MemberExpression)?.Member ?? 
-        throw new Exception("Wrong expression type. Needed member acces expression");
+    var arguments = (selector.Body.ReduceExtensions() as NewExpression)?.Arguments ?? 
+        throw new Exception("Sequence returns no new types");
 
-    var columnName = member.GetCustomAttribute<ColumnAttribute>()?.Name ?? member.Name;
+    var members = arguments.ToList().ConvertAll(e => (e as MemberExpression)?.Member ?? 
+        throw new Exception("Sequence contains no elements"));
+
+    var columns = members.ConvertAll(e => e.GetCustomAttribute<ColumnAttribute>()?.Name ?? e.Name);
+
+    using var npgsqlCommandInstance = new NpgsqlCommand();
+
+    string query = $"SELECT {columns.Aggregate((columns, next) => columns += $", {next}")} ";
+
+    string fromQuery = "FROM gyms";
+
+    npgsqlCommandInstance.CommandText = query + fromQuery;
+    npgsqlCommandInstance.Connection = connection;
 
     connection.Open();
 
-    string query = $"SELECT {columnName}";
+    using var reader = npgsqlCommandInstance.ExecuteReader();
+
+    var anonType = selector.ReturnType.GetConstructors().FirstOrDefault();
+
+    while (reader.Read())
+    {
+        List<object?> row = new List<object?>();
+
+        foreach (var columnName in columns)
+        {
+            row.Add(reader[columnName]);
+        }
+
+        result.Add((TResult)anonType.Invoke(row.ToArray()));
+    }
 
     connection.Close();
 
-    return default;
+    return result;
 }
 //TableDefinition<Gym> gyms = new TableDefinition<Gym>(connection);
 
